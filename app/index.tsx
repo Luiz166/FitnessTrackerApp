8549d79d56e8283343ebebc7bcc7a708ac3a2fdc
Luiz166 <otavio.tata.morais@gmail.com>
2024-11-13 16:13:48 -0300

    main page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.h1}>Fitfy</Text>
+      <Text style={styles.h1}>Preparado para conquistar seu corpo dos sonhos?</Text>
+      <Link href="/screens/login">Sim</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  h1: {
+    fontSize: 24,
+    fontWeight: "700",
+    marginBottom: 20
+  }
+})
