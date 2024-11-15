aa0f4addeb3c602b6877e23204c400507743bb9d
Luiz166 <otavio.tata.morais@gmail.com>
2024-11-15 17:03:59 -0300

    changing launch screen, add fucntion to push onboard page after 3 sec

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,19 @@
-import { Link } from "expo-router";
-import { Text, View, StyleSheet } from "react-native";
+import { router } from "expo-router";
+import { useEffect } from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
 
 export default function Index() {
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/screens/onBoard")
+    }, 3000)
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text style={styles.h1}>Fitfy</Text>
-      <Text style={styles.h1}>Preparado para conquistar seu corpo dos sonhos?</Text>
-      <Link href="/screens/login">Sim</Link>
+      <Image style={styles.img} source={require('../assets/images/logo.png')}/>
+      <Text style={styles.h1}>fitfy</Text>
     </View>
   );
 }
@@ -15,12 +22,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    backgroundColor: "#232323",
   },
   
   h1: {
-    fontSize: 24,
+    fontSize: 32,
     fontWeight: "700",
-    marginTop: 20
+    color: "#E2F163",
+    textTransform: "uppercase",
+    letterSpacing: 2,
+  },
+  img: {
+    maxHeight: 200
   }
 })
