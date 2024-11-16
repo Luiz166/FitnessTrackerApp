9478ba50b1a446ce2adf09ff6054c2f85c3e2d27
Luiz166 <otavio.tata.morais@gmail.com>
2024-11-16 03:18:23 -0300

    add cors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 import express from "express"
 import userRoute from './routes/user.js';
 import 'dotenv/config'
+import cors from "cors"
 
 const PORT = process.env.PORT
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/user", userRoute);
 
