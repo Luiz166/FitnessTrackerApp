cecfcec972278faa55d06c0f108c9a0c689ef6b4
Luiz166 <otavio.tata.morais@gmail.com>
2024-11-16 03:19:10 -0300

    add login route, check if is user already before registering

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,16 +1,45 @@
 import express from "express"
 import database from "../database.js";
+import bcrypt from "bcrypt"
 
 const router = express.Router();
 
-router.post('/create', (req, res) => {
-    const query = 'INSERT INTO users (name, email, password) values (?, ?, ?)';
-    database.query(query, [req.user], (err) => {
-        if(err){
-            console.error('Erro ao adicionar usuario: ', err);
-            return res.status(500).json({message: 'Erro ao adicionar usuário'});
+router.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+
+    const isUserAlready = "SELECT * FROM users WHERE email = ?";
+    database.query(isUserAlready, [email], async (err, results) => {
+        if(err) return res.status(500).json({message: 'Erro ao verificar se usuario existe'})
+        if(results.length > 0){
+            return res.status(400).json({message: 'Usuario ja existe'})
         }
-        res.status(200).json({message: 'Usuario adicionado com sucesso'});
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const query = 'INSERT INTO users (name, email, password) values (?, ?, ?)';
+        database.query(query, [name, email, hashedPassword], (err) => {
+            if(err){
+                console.error('Erro ao adicionar usuario: ', err);
+                return res.status(500).json({message: 'Erro ao adicionar usuário'});
+            }
+            res.status(200).json({message: 'Usuario cadastrado com sucesso'});
+        })
+    })
+})
+
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    //checar se o usuario existe
+    const isUserReal = "SELECT * FROM users WHERE email = ?";
+    database.query(isUserReal, [email], async (err, results) => {
+        if(err) return res.status(500).json({message: 'Erro ao verificar se usuario existe'})
+        if(results.length > 0){
+            //checar se a senha é valida
+            const user = results[0]
+            const isPasswordValid = await bcrypt.compare(password, user.password);
+            if(!isPasswordValid) return res.status(400).json({message: "Senha invalida"})
+            return res.status(200).json({message: "Logado com sucesso"})
+        }
     })
 })
 
