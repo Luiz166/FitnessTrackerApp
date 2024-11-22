4d03ac43bed911cb45f8372d3d2ea7c5c42f008e
Luiz166 <otavio.tata.morais@gmail.com>
2024-11-22 19:27:21 -0300

    adding more arguments to saveToken

--- app/auth/AuthProvider.tsx
+++ app/auth/AuthProvider.tsx
@@ -1,18 +1,19 @@
 import * as SecureStore from 'expo-secure-store'
 
-export async function saveToken(token: string){
+export async function saveToken(token: string, username: string){
     try{
         await SecureStore.setItemAsync('userToken', token);
+        await SecureStore.setItemAsync('username', username);
         console.log('token saved')
     }catch(error){
         console.error("Error saving token: ", error)
     }
 }
 
-export async function getToken() {
+export async function getToken(key : string) {
     try{
-        const token = await SecureStore.getItemAsync('userToken');
-        console.log('token:', token )
+        const token = await SecureStore.getItemAsync(key);
+        console.log(key , token )
         return token
     }catch(err){
         console.error("Error retrieving token: ", err)
