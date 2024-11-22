8c9769766013668a1e4c063baa3af72eff08c03c
Luiz166 <otavio.tata.morais@gmail.com>
2024-11-22 19:28:59 -0300

    change status bar, disable navigation bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,35 @@
 
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
-
+import { StatusBar } from "expo-status-bar";
+import * as NavigationBar from 'expo-navigation-bar'
+import { useEffect } from "react";
+import { AppState } from "react-native";
 
 export default function RootLayout() {
+  useEffect(() => {
+    const handleAppStateChange = async (nextAppState: string) => {
+      if(nextAppState === "active"){
+        await NavigationBar.setVisibilityAsync("hidden")
+      }
+    }
+    const subscription = AppState.addEventListener("change", handleAppStateChange)
+
+    NavigationBar.setVisibilityAsync("hidden")
+
+    return() => {
+      subscription.remove();
+    };
+  }, [])
   return(
-    <Stack
-      screenOptions={{
-        headerShown: false
-      }}
-    />
+    <ThemeProvider value={DarkTheme}>
+      <Stack
+        screenOptions={{
+          headerShown: false
+        }}
+        >
+      </Stack>
+      <StatusBar style="auto"/>
+    </ThemeProvider>
   )
 }
